3e3123731c0527ce871ec4d77f8ee54d9cd1e41a
zeokin <vexel125@outlook.com>
2026-03-18 23:45:15 -0500

    init

---
+++ src-tauri/gen/android/app/src/main/java/com/uni/npt_mobile_wallet/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.uni.npt_mobile_wallet
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
